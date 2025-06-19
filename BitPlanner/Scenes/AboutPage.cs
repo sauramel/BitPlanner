3c9df4ff9f2cdc1b50a5eab6e52b2ca8108644f4
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2025-06-19 06:31:46 +0300

    Make about page scrollable
    Required to correctly show everything in a small window

--- BitPlanner/Scenes/AboutPage.cs
+++ BitPlanner/Scenes/AboutPage.cs
@@ -9,10 +9,10 @@ public partial class AboutPage : PanelContainer, IPage
     public override void _Ready()
     {
         var appVersion = ProjectSettings.GetSetting("application/config/version");
-        GetNode<Label>("MarginContainer/VBoxContainer/AppVersion").Text = $"Version {appVersion}";
+        GetNode<Label>("ScrollContainer/MarginContainer/VBoxContainer/AppVersion").Text = $"Version {appVersion}";
 
         using var dataVersionFile = FileAccess.Open("res://data_version.txt", FileAccess.ModeFlags.Read);
         var dataVersion = DateOnly.Parse(dataVersionFile.GetAsText(), CultureInfo.InvariantCulture);
-        GetNode<Label>("MarginContainer/VBoxContainer/DataVersion").Text = $"Game data as of {dataVersion}";
+        GetNode<Label>("ScrollContainer/MarginContainer/VBoxContainer/DataVersion").Text = $"Game data as of {dataVersion}";
     }
 }
